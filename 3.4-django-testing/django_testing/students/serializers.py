c9b784b9ca76211d74ad0adee324f7cc10459951
Andrey Shishov <a.5920311@yandex.ru>
2022-12-11 22:22:19 +0300

    ready

--- 3.4-django-testing/django_testing/students/serializers.py
+++ 3.4-django-testing/django_testing/students/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.settings import settings
 
 from students.models import Course
 
@@ -8,3 +9,9 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ("id", "name", "students")
+
+    def validate_students(self, value):
+        q = Course.objects.filter(students=value).count()
+        max_students_per_course = settings('MAX_STUDENTS_PER_COURSE')
+        if q >= max_students_per_course:
+            raise serializers.ValidationError('Достигнут лимит студентов на курсе')
